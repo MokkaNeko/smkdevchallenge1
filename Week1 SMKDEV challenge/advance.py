073a7e9ef9e32c4385e192fa1b63312beb5fd95e
iksanard <105214124+MokkaNeko@users.noreply.github.com>
2023-08-22 21:37:51 +0700

    Update advance.py

--- Week1 SMKDEV challenge/advance.py
+++ Week1 SMKDEV challenge/advance.py
@@ -8,7 +8,7 @@
 # 1
 # Write a program to use for loop to print the following reverse number pattern
 
-for i in range(5, 0, -1):
-    for j in range(i, 0, -1):
-        print(j, end=" ")
-    print()
+for i in range(5, 0, -1):            # for loop to initialize row
+    for j in range(i, 0, -1):        # for loop to initialize column
+        print(j, end=" ")            # print the number(s)
+    print()                          # end the column, so we have a new row
